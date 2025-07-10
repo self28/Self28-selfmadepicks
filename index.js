7db4b3d340ab94510cda55bc44a19def2f02fa75
self28 <selfescape9428@gmail.com>
2025-07-10 01:59:20 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Welcome to SelfMadePicks!</h1>
-    </div>
-  );
-}
